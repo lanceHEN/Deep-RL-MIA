5b548350d63fc5b903e09a13117799e9c4395f7d
lancehendricks <hendricks.la@northeastern.edu>
2026-01-26 20:38:54 -0500

    Reworked experiment runner to work with other external trajectory method

--- src/experiment/experiment_runner.py
+++ src/experiment/experiment_runner.py
@@ -63,7 +63,7 @@ class ExperimentRunner:
         )
 
         # Collect training trajectories
-        train_trajectories = data_oracle.collect_trajectories(
+        train_trajectories = data_oracle.collect_training_trajectories(
             self.config.train_trajs, self.config.T_max, self.config.train_seed
         )
 
@@ -81,8 +81,8 @@ class ExperimentRunner:
         trainer_oracle.train(train_trajectories, epochs=2)
 
         # Get some external trajectories - use train_trajectories to ensure different.
-        external_trajectories = data_oracle.collect_trajectories(
-            self.config.external_trajs, self.config.T_max, self.config.external_seed
+        external_trajectories = data_oracle.collect_external_trajectories(
+            train_trajectories, self.config.external_trajs, self.config.T_max, self.config.external_seed
         )
 
         # Get output trajectories from trainer oracle.
@@ -129,6 +129,19 @@ class ExperimentRunner:
         all_pairs = np.vstack((positive_pairs, negative_pairs))
         all_labels = np.concatenate((positive_labels, negative_labels))
         
+        # Batch if necessary if in collective mode
+        if not self.config.individual_attack:
+            # [train_trajs + external_trajs, 2*action_dim, T_max] -> 
+            # [(train_trajs + external_trajs) // collective_batch_size, collective_batch_size, 2*action_dim, T_max]
+            m = self.config.collective_batch_size
+            all_pairs = np.array([all_pairs[i:i+ m] for i in range(0, (len(all_pairs) // m) * m, m)])
+            
+            # [train_trajs + external_trajs,] -> [(train_trajs + external_trajs) // collective_batch_size, collective_batch_size]
+            all_labels = np.array([all_labels[i:i+ m] for i in range(0, (len(all_labels) // m) * m, m)])
+            
+            # Tranpose so minibatch size is final dim, as done in the paper
+            all_pairs = np.transpose(all_pairs, (0, 2, 3, 1))
+        
         train_pairs, test_pairs, train_labels, test_labels = train_test_split(all_pairs, all_labels, test_size=self.config.attack_trainer_train_test_split_test_size,
                                                                               random_state=self.config.attack_trainer_train_test_split_seed)
 
@@ -149,41 +162,44 @@ class ExperimentRunner:
 
         test_preds = attack_trainer.predict(test_pairs)
         
-        print(test_labels.shape)
-        print(test_preds.shape)
-        
         print(f"Accuracy: {100*accuracy_score(test_labels, test_preds)}%")
 
+def main():
 
-attack_trainer_config = IndividualAttackTrainerConfig(
-    action_dim=3,
-    T_max=100,
-    classification_threshold=0.5
-)
+    attack_trainer_config = IndividualAttackTrainerConfig(
+        action_dim=3,
+        T_max=100,
+        classification_threshold=0.5
+    )
 
-config = ExperimentConfig(
-    individual_attack=True,
-    attack_trainer_config=attack_trainer_config,
-    env=gym.make("Hopper-v5"),
-    T_max=100,
-    train_trajs=1000,
-    train_seed=1,
-    external_trajs=1000,
-    external_seed=2,
-    output_seed=3,
-    attack_trainer_epochs=100,
-    attack_trainer_train_test_split_seed=4,
-    attack_trainer_train_test_split_test_size=0.2,
-    attack_trainer_batch_size=16,
-    data_oracle_ddpg_verbose=0,
-    data_oracle_ddpg_buffer_size=10000,
-    data_oracle_ddpg_learning_starts=1000,
-    data_oracle_ddpg_learn_timesteps=200000,
-    trainer_oracle_bcq_device="cpu:0",
-    trainer_oracle_bcq_batch_size=100,
-    trainer_oracle_discount_factor=0.99,
-)
+    config = ExperimentConfig(
+        individual_attack=True,
+        attack_trainer_config=attack_trainer_config,
+        collective_batch_size=50,
+        env=gym.make("Hopper-v5"),
+        T_max=100,
+        train_trajs=1000,
+        train_seed=1,
+        external_trajs=1000,
+        external_seed=2,
+        output_seed=3,
+        attack_trainer_epochs=100,
+        attack_trainer_train_test_split_seed=4,
+        attack_trainer_train_test_split_test_size=0.2,
+        attack_trainer_batch_size=16,
+        data_oracle_ddpg_verbose=0,
+        data_oracle_ddpg_buffer_size=10000,
+        data_oracle_ddpg_learning_starts=10,
+        data_oracle_ddpg_learn_timesteps=10,
+        trainer_oracle_bcq_epochs=2,
+        trainer_oracle_bcq_device="cpu:0",
+        trainer_oracle_bcq_batch_size=100,
+        trainer_oracle_discount_factor=0.99,
+    )
 
-experiment_runner = ExperimentRunner(config)
+    experiment_runner = ExperimentRunner(config)
 
-experiment_runner.run_experiment()
+    experiment_runner.run_experiment()
+    
+if __name__ == "__main__":
+    main()
