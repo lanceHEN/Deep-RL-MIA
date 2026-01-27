121f789b48d791ab1071853259ab16ac6d0b38ca
lancehendricks <hendricks.la@northeastern.edu>
2026-01-26 20:38:06 -0500

    Added separate method for external trajectory generation

--- src/model/data_oracle.py
+++ src/model/data_oracle.py
@@ -11,7 +11,9 @@ class DataOracle:
     """
     An implementation of the Data Oracle. This will interact with a
     specified environment and return i.i.d. trajectories for use by the
-    Model Trainer Oracle, via the collect_trajectories method.
+    Model Trainer Oracle, via the collect_training_trajectories method. It
+    can also return external trajectories that are explicitly different
+    from the training trajectories via the collect_external_trajectories method.
 
     Here, we use DDPG as the exploration policy.
 
@@ -47,8 +49,43 @@ class DataOracle:
             total_timesteps=self.config.ddpg_learn_timesteps, progress_bar=True
         )
         print("Finished learning DDPG policy")
+        
+    def _gen_trajectory(self, T_max: int) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Generates a single trajectory, returning numpy arrays for states, actions,
+        and rewards.
+        """
+        states = []
+        actions = []
+        rewards = []
 
-    def collect_trajectories(
+        current_state = self.env.reset()[0]  # Always start a new episode.
+
+        for _ in range(T_max):
+            # print(current_state)
+            # Get the action
+            action, _ = self.policy.predict(current_state)
+
+            # Step through environment
+            next_state, reward, done, _, _ = self.env.step(action)
+
+            states.append(current_state)
+            actions.append(action)
+            rewards.append(reward)
+
+            if done:
+                states.append(next_state)
+                break
+
+            current_state = next_state
+                
+        states = np.array(states)
+        actions = np.array(actions)
+        rewards = np.array(rewards)
+            
+        return states, actions, rewards
+
+    def collect_training_trajectories(
         self, n_trajectories: int, T_max: int, seed: int = None
     ) -> List[Dict[str, np.ndarray]]:
         """
@@ -67,50 +104,95 @@ class DataOracle:
                 a 'rewards' key mapping to a [T,] array of rewards, and a 'terminals'
                 key mapping to whether each transition was the last one or not.
         """
-        print(f"Generating {n_trajectories} trajectories.")
+        print(f"Generating {n_trajectories} training trajectories.")
         if seed is not None:  # For reuse
             self.env.reset(seed=seed)
             np.random.seed(seed)
 
         trajectories = []
         for _ in range(n_trajectories):
-            states = []
-            actions = []
-            rewards = []
-
-            current_state = self.env.reset()[0]  # Always start a new episode.
-
-            for _ in range(T_max):
-                # print(current_state)
-                # Get the action
-                action, _ = self.policy.predict(current_state)
-
-                # Step through environment
-                next_state, reward, done, _, _ = self.env.step(action)
-
-                states.append(current_state)
-                actions.append(action)
-                rewards.append(reward)
-
-                if done:
-                    states.append(next_state)
-                    break
-
-                current_state = next_state
+            states, actions, rewards = self._gen_trajectory(T_max)
 
             # Create terminal flags array
             T = len(actions)
             terminals = np.zeros(T, dtype=np.float32)
             terminals[-1] = 1.0
+            
 
             trajectories.append(
                 {
-                    "states": np.array(states),  # [T, state_dim]
-                    "actions": np.array(actions),  # [T, action_dim]
-                    "rewards": np.array(rewards),  # [T,]
+                    "states": states,  # [T, state_dim]
+                    "actions": actions,  # [T, action_dim]
+                    "rewards": rewards,  # [T,]
                     "terminals": terminals,  # [T,]
                 }
             )
 
-        print("Finished generating trajectories.")
+        print("Finished generating training trajectories.")
+        return trajectories
+
+    def collect_external_trajectories(
+        self,
+        train_trajectories: List[Dict[str, np.ndarray]],
+        n_trajectories: int,
+        T_max: int,
+        seed: int = None
+    ) -> List[Dict[str, np.ndarray]]:
+        """
+        Performs the same as collect_training_trajectories, except it also
+        checks, for each outputted trajectory, that it is not a duplicate of
+        a trajectory in train_trajectories (according to states and actions).
+
+        Args:
+            train_trajectories (List[Dict[str, np.ndarray]]): List of training
+                trajectories to ensure no duplicates are made from them.
+            n_trajectories (int): Number of trajectories to generate.
+            T_max (int): Maximal number of steps in a trajectory.
+            seed (int): Optional random seed for reproducibility.
+
+        Returns:
+            List[Dict[str, np.ndarray]]: List of trajectories different from
+                anything in train_trajectories by at least one state or action, where each trajectory
+                contains an a 'states' key mapping to a [T, state_dim] array of states,
+                a 'actions' key mapping to a [T, action_dim] array of actions,
+                a 'rewards' key mapping to a [T,] array of rewards, and a 'terminals'
+                key mapping to whether each transition was the last one or not.
+        """
+        print(f"Generating {n_trajectories} external trajectories.")
+        if seed is not None:  # For reuse
+            self.env.reset(seed=seed)
+            np.random.seed(seed)
+
+        trajectories = []
+        
+        while len(trajectories) < n_trajectories:
+            states, actions, rewards = self._gen_trajectory(T_max)
+                
+            skip = False
+            # Check not in train_trajectories
+            for train_traj in train_trajectories:
+                if len(train_traj["actions"]) == len(actions):
+                    if np.allclose(actions, train_traj["actions"], atol=1e-6) and np.allclose(states, train_traj["states"], atol=1e-6):
+                        skip = True
+                        print("Skipping generated trajectory because it is identical to a training one")
+                        break
+                        
+            if skip:
+                continue
+
+            # Create terminal flags array
+            T = len(actions)
+            terminals = np.zeros(T, dtype=np.float32)
+            terminals[-1] = 1.0  
+
+            trajectories.append(
+                {
+                    "states": states,  # [T, state_dim]
+                    "actions": actions,  # [T, action_dim]
+                    "rewards": rewards,  # [T,]
+                    "terminals": terminals,  # [T,]
+                }
+            )
+
+        print("Finished generating external trajectories.")
         return trajectories
